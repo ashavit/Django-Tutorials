4eb45e6c8f0ca8e8eeacb91ea4c30663baee56a8
Amir Shavit <amir.shavit@autodesk.com>
2015-10-20 14:39:33 +0300

    Use generic views, https://docs.djangoproject.com/en/1.7/intro/tutorial04/#use-generic-views-less-code-is-better

--- polling_apps/polls/urls.py
+++ polling_apps/polls/urls.py
@@ -1,4 +1,4 @@
-__author__ = 'AmirShavit'
+__author__ = 'Amir Shavit'
 
 from django.conf.urls import patterns, url
 
@@ -6,11 +6,11 @@ from polls import  views
 
 urlpatterns = patterns('',
                        # ex: /polls/
-                       url(r'^$',views.index, name='index'),
+                       url(r'^$', views.IndexView.as_view(), name='index'),
                        # ex: /polls/5/
-                       url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+                       url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
                        # ex: /polls/5/result/
-                       url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+                       url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
                        # ex: /polls/5/vote/
                        url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
                        )
